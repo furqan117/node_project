ee2eb0626edc5d5a439d55c1ff7517d1cd8963e3
furqan117 <furqaanfarxaan2@gmail.com>
2024-11-02 21:16:45 +0300

    fileHandler.js

--- utils/fileHandler.js
+++ utils/fileHandler.js
@@ -1,14 +1,19 @@
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
 
 const filePath = './data/tasks.json';
 
-exports.writeTasksTofile = (tasks) => {
-    fs.writeFileSync(filePath, JSON.stringify(tasks))
+exports.writeTasksToFile = (tasks) => {
+    if (Array.isArray(tasks) && tasks.length === 0) {
+        fs.writeFileSync(filePath, '[]');
+    } else {
+        fs.writeFileSync(filePath, JSON.stringify(tasks, null, 2));
+    }
 }
+
 exports.readTasksFromFile = () => {
     if(!fs.existsSync(filePath)) {
-        writeTasksTofile([])
+        this.writeTasksToFile([])
     }
 
     const data = fs.readFileSync(filePath);
