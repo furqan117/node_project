0a83bb11774b1b816a01e135b8c54c8ad7e1680b
furqan117 <furqaanfarxaan2@gmail.com>
2024-11-02 21:15:52 +0300

    taskRoutes.js

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,21 +1,20 @@
-const { getTasks, createTask, updateTask, deleteTask } = require("../controllers/taskControlers");
+const { getTasks, createTask, updateTask, deleteTask } = require("../controllers/taskControllers");
 
 const taskRoutes = (req, res) => {
-    if(req.method = 'GET') {
-        getTasksASKS(req, res);
-    } else if(req.method = 'POST') {
-        createTaskTask(req, res)
-    } else if(req.method = 'PATCH') {
+    if(req.method === 'GET') {
+        getTasks(req, res);
+    } else if(req.method === 'POST') {
+        createTask(req, res)
+    } else if (req.method === 'PATCH') {
         updateTask(req, res)
-    } else if(req.method = 'DELETE') {
+    } else if(req.method === 'DELETE') {
         deleteTask(req, res)
     } else {
-        res.writeHead(404, 'data not found', {'content-type': 'application/json'})
-        res.end(json.stringfy({
-            message: "unknown method required."
+        res.writeHead(404, 'Data Not Found', { 'content-type': 'application/json'})
+        res.end(JSON.stringify({
+            message: "Unknown Method required."
         }))
     }
 }
 
 module.exports = taskRoutes;
-
