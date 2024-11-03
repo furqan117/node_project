b8e1ed33c9364aeb0b7e702b2caf37aaa0e4caff
furqan117 <furqaanfarxaan2@gmail.com>
2024-11-03 10:14:02 +0300

    app.js

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const http = require('http');
-const taskRoutes = require('./routes/taskRoutes');
+const taskRoutes = require('./routes/taskRouter');
 
-const HOSTNAME = 'localhost'
-const PORT = 9000
+const HOSTNAME = 'localhost';
+const PORT = 9000;
 
 const server = http.createServer((req, res) => {
     if (req.url.startsWith('/tasks')) {
-        taskRoutes(req, res)
+        taskRoutes(req, res);
     } else {
-        res.writeHead(404, 'Not Found', { 'content-type': 'application/json'})
-        res.end(JSON.stringify({
-            message: 'Sorry, you got lost!'
-        }))
+        res.writeHead(404, { 'content-type': 'application/json' });
+        res.end(JSON.stringify({ message: 'Sorry, you got lost!' }));
     }
 });
 
 server.listen(PORT, HOSTNAME, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on http://${HOSTNAME}:${PORT}`);
+});
